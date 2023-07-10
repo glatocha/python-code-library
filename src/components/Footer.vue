14bc3f5a22cf09b4c1998ef0f82c83b848a1a72e
glatocha <glatocha@gmail.com>
2023-07-10 11:35:31 +0200

    Added highlight.js to footer with animation

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,11 +2,38 @@
   <div class="bg-opacity-10 bg-white py-1">
     <p class="text-gray-400 text-sm font-bold">
       &copy; <a href="mailto:glatocha@gmail.com">Grzegorz Latocha</a> @2020-2023 - powered by
-      <a href="https://supabase.com/" target="blank">Supabase</a>
+      <a href="https://supabase.com/" target="blank">Supabase</a>. Utilizing
+      <a :class="hljs_class" href="https://highlightjs.org/" target="blank">highlight.js</a>
     </p>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+var counter = 0;
+const hljs_class = ref("hljs-comment");
+const hljs_classes = [
+  "hljs-comment",
+  "hljs-attr",
+  "hljs-attribute",
+  "hljs-name",
+  "hljs-selector-class",
+  "hljs-link",
+  "hljs-meta",
+  "hljs-literal",
+  "hljs-code",
+  "hljs-meta",
+  "hljs-deletion",
+  "hljs-addition",
+];
+setInterval(() => {
+  counter++;
+  if (counter > 11) {
+    counter = 0;
+  }
+  console.log("Footer loop - " + hljs_classes[counter]);
+  hljs_class.value = hljs_classes[counter];
+}, 1000);
+</script>
 
 <style scoped></style>
